4ee2f88adf9e5d2f64b5d7dc14246286eca906c9
Alex Ioja-Yang <alexiojayang@gmail.com>
2023-07-14 08:47:02 +0100

    Initial Commit

---
+++ ExtensionsBehaviourImpact/ExtensionsBehaviourImpactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtensionsBehaviourImpactApp.swift
+//  ExtensionsBehaviourImpact
+//
+//  Created by Alex Ioja-Yang on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExtensionsBehaviourImpactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
